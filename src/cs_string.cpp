059e8e24380969a81343879aade59e3cd56d2d42
Barbara Geller <barbara@copperspice.com>
2018-01-01 14:22:25 -0800

    Update copyright

--- src/cs_string.cpp
+++ src/cs_string.cpp
@@ -1,12 +1,12 @@
 /***********************************************************************
 *
-* Copyright (c) 2017-2017 Barbara Geller
-* Copyright (c) 2017-2017 Ansel Sermersheim
+* Copyright (c) 2017-2018 Barbara Geller
+* Copyright (c) 2017-2018 Ansel Sermersheim
 * All rights reserved.
 *
-* This file is part of libCsString
+* This file is part of CsString
 *
-* libCsString is free software, released under the BSD 2-Clause license.
+* CsString is free software, released under the BSD 2-Clause license.
 * For license details refer to LICENSE provided with this project.
 *
 ***********************************************************************/
