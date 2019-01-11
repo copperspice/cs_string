8f927dd7d6e0bbee339e3cd88bb29039b9cdcdda
Barbara Geller <barbara@copperspice.com>
2019-01-11 11:18:31 -0800

    Update copyright

--- src/cs_string.cpp
+++ src/cs_string.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 *
-* Copyright (c) 2017-2018 Barbara Geller
-* Copyright (c) 2017-2018 Ansel Sermersheim
+* Copyright (c) 2017-2019 Barbara Geller
+* Copyright (c) 2017-2019 Ansel Sermersheim
 * All rights reserved.
 *
 * This file is part of CsString
