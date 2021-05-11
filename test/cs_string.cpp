730e7e6284753845b2d52282e6b795d1a31f3daf
Barbara Geller <barbara@copperspice.com>
2021-05-11 12:31:10 -0800

    Add empty cs_string test file

---
+++ test/cs_string.cpp
@@ -0,0 +1,24 @@
+/***********************************************************************
+*
+* Copyright (c) 2012-2021 Barbara Geller
+* Copyright (c) 2012-2021 Ansel Sermersheim
+*
+* This file is part of CopperSpice.
+*
+* CopperSpice is free software. You can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* version 2.1 as published by the Free Software Foundation.
+*
+* CopperSpice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://www.gnu.org/licenses/
+*
+***********************************************************************/
+
+#define CS_STRING_ALLOW_UNSAFE
+
+#include <cs_string.h>
+
+#include <cs_catch2.h>
