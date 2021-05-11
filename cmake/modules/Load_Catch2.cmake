f5d8b0ef1a91a1536dc0cadaacbb332f5cdcec82
Barbara Geller <barbara@copperspice.com>
2021-05-11 10:32:56 -0800

    Add Catch2 CMake macros

---
+++ cmake/modules/Load_Catch2.cmake
@@ -0,0 +1,25 @@
+# ***********************************************************************
+#
+# Copyright (c) 2017-2021 Barbara Geller
+# Copyright (c) 2017-2021 Ansel Sermersheim
+#
+# This file is part of CsString.
+#
+# CsString is free software. You can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# CopperSpice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# https://www.gnu.org/licenses/
+#
+# ***********************************************************************
+
+find_package(Catch2 QUIET)
+
+if (NOT TARGET Catch2::Catch2)
+   message(STATUS "Catch2 was not found, CsString unit tests will not be built\n")
+   return()
+endif()
